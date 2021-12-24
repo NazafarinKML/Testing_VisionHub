2bde71a3b20e32808051d40e3bd5c1544e8197db
NKML <nazi.kml70@gmail.com>
2021-12-24 19:19:11 +0330

    mount

--- src/pages/login.spec.ts
+++ src/pages/login.spec.ts
@@ -1,27 +1,33 @@
-import { test, expect } from '@playwright/test';
-
-/*test('basic test', async ({ page }) => {
-  await page.goto('http://showroom.arivis.com/');
-
-});*/
+import { test, expect, chromium } from '@playwright/test';
 
 
-test.beforeEach(async ({ page }) => {
-    // Runs before each test and signs in each page.
+//Login test
+
+test('login' , async ({page}) => {
+    
+  
     await page.goto('https://showroom.arivis.com/#/login');
-   // await page.click('text=Login');
     await page.fill('#input-15', 'nazafarin');
     await page.fill('#input-18', 'arivis');
     await page.click('text=LOGIN');
-  });
-  
-  test('first', async ({ page }) => {
-    // page is signed in.
+    await expect(page).toHaveURL('https://showroom.arivis.com/#/');
+    await page.click('div.v-card__title');
+    await expect(page).toHaveURL('https://showroom.arivis.com/#/datasets');
+
+    await page.click('button.a-toolbar_left-action-menu_activator');
+    await page.click('text=Mount existing dataset');
+    await expect(page).toHaveURL('https://showroom.arivis.com/#/datasets/mount');
+    await page.dblclick('text=_testdata');
+    await page.dblclick('text=_nativeformats');
+    await page.dblclick('text=czi');
+    await page.dblclick('text=hcs');
+    await page.click('text=next');
+    await page.click('text=MOUNT DATASETS');
+    await page.waitForSelector('span.success');
+    await page.goto('https://showroom.arivis.com/#/datasets');
+    await page.screenshot({ path: 'screenshot1.png'});
+
     
-    //await page.screenshot({ path: 'screenshot.png' });
-    await page.locator('#submit').click();
-  });
+});
   
-/* test('second', async ({ page }) => {
-    // page is signed in.
-  }); */
+  
