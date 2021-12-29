4a234493175c76d951f815774164b18433c3b50b
NKML <nazi.kml70@gmail.com>
2021-12-29 12:04:51 +0330

    viewer

--- src/pages/login.spec.ts
+++ src/pages/login.spec.ts
@@ -1,8 +1,87 @@
 import { test, expect, chromium } from '@playwright/test';
 
+test.beforeEach( async ( {page} )=> {
+
+    const browser= await chromium.launch();
+    const context=await browser.newContext();
+    
+    await page.goto('https://showroom.arivis.com/#/login');
+    await page.fill('#input-15', 'nazafarin');
+    await page.fill('#input-18', 'arivis');
+    await page.click('text=LOGIN');
+    await expect(page).toHaveURL('https://showroom.arivis.com/#/');
+    await page.click('div.v-card__title');
+    await expect(page).toHaveURL('https://showroom.arivis.com/#/datasets');
+
+});
+
+
+/*
+test.describe( 'Mount an exsiting dataset', () => {
+
+    test('Mount a dataset' , async({page}) => {
+
+        await page.click('div.v-card__title');
+        await page.click('button.a-toolbar_left-action-menu_activator');
+        await page.click('text=Mount existing dataset');
+        await page.dblclick('text=_testdata');
+        await page.dblclick('text=_nativeformats');
+        await page.dblclick('text=czi');
+        await page.dblclick('text=hcs');
+        await page.click('text=next');
+        await page.click('text=MOUNT DATASETS');
+        await page.waitForSelector('span.success');
+
+    });
+
+    test('Search new registered dataset', async({page}) => {
+
+        await page.click('div.v-card__title');
+        await page.goto('https://showroom.arivis.com/#/datasets');
+
+    });
+
+});
+*/
+
+test('Open Viwewer For Dataset' , async ({page}) => {
+
+    await page.dblclick('text=hcs');
+    await page.click('i.fa-palette');
+    await page.waitForSelector('div.iv_color-visibility__color');
+    const elements = await page.$$('div.iv_color-visibility__color');
+    const elementsItem1= await elements[0].textContent();
+    expect(elementsItem1).toBe(' DAPI ');
+    const elementsItem2= await elements[1].textContent();
+     expect(elementsItem2).toBe(' EGFP ');
+    
+
+});
+
+test.afterAll( async({browser}) => {
+
+    await browser.close();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //Login test
 
+/*
 test('login' , async ({page}) => {
     
   
@@ -25,9 +104,14 @@ test('login' , async ({page}) => {
     await page.click('text=MOUNT DATASETS');
     await page.waitForSelector('span.success');
     await page.goto('https://showroom.arivis.com/#/datasets');
-    await page.screenshot({ path: 'screenshot1.png'});
+    
+    await page.dblclick('text=hcs');
+    await page.dblclick('text=hcs');
+    await expect(page).toHaveURL(imgUrl);
+    await page.click('i.fa-palette');
+    await page.screenshot({ path: 'screenshot5.png'});
 
     
-});
+}); */
   
   
