8c13de06b2c671a14c0545749cbc960276101c74
NKML <nazi.kml70@gmail.com>
2022-01-07 13:36:27 +0330

    project compleated

--- src/pages/login_and_GroupTests.spec.ts
+++ src/pages/login_and_GroupTests.spec.ts
@@ -1,28 +1,37 @@
 import { test, expect, chromium } from '@playwright/test';
 
+
+// Login Befor Each Test
+
 test.beforeEach( async ( {page} )=> {
 
     const browser= await chromium.launch();
     const context=await browser.newContext();
-    
     await page.goto('https://showroom.arivis.com/#/login');
     await page.fill('#input-15', 'nazafarin');
     await page.fill('#input-18', 'arivis');
     await page.click('text=LOGIN');
     await expect(page).toHaveURL('https://showroom.arivis.com/#/');
-    await page.click('div.v-card__title');
-    await expect(page).toHaveURL('https://showroom.arivis.com/#/datasets');
+
+    await Promise.all([
+        page.waitForNavigation({ url: 'https://showroom.arivis.com/#/datasets' }),
+        page.click('text=Datasets Manage your datasets. >> div')
+      ]);
 
 });
 
-
+//Group Test: Mount, Chanel Test, Search, Delete, Change plane
 
 test.describe( 'Mount an exsiting dataset', () => {
 
     test('Mount a dataset' , async({page}) => {
 
-        await page.click('button.a-toolbar_left-action-menu_activator');
-        await page.click('text=Mount existing dataset');
+        await page.click('main span >> :nth-match(button, 2)');
+        await Promise.all([
+            page.waitForNavigation({ url: 'https://showroom.arivis.com/#/datasets/mount' }),
+            page.click('text=Mount existing dataset')
+          ]);
+
         await page.dblclick('text=_testdata');
         await page.dblclick('text=_nativeformats');
         await page.dblclick('text=czi');
@@ -38,7 +47,7 @@ test.describe( 'Mount an exsiting dataset', () => {
 
         await page.fill('[placeholder="search..."]' , 'hcs');
         await page.click('a[title=hcs]');
-        await page.screenshot({ path: 'screenshot6.png'});
+        
 
     }); 
 
@@ -51,7 +60,7 @@ test.describe( 'Mount an exsiting dataset', () => {
 
     })
 
-  /*  test('Delete Dataset by ctx menu' , async ({page}) => {
+    test('Delete Dataset by ctx menu' , async ({page}) => {
 
 
         await page.click('text = hcs');
@@ -60,7 +69,7 @@ test.describe( 'Mount an exsiting dataset', () => {
         await page.click('text=yes');
 
 
-    }); */
+    });
 
 });
 
@@ -79,60 +88,35 @@ test('Open Viewer For Dataset' , async ({page}) => {
 
 });
 
-test.afterAll( async({browser}) => {
+test('changing current plane' , async ({page}) => {
 
+    await page.dblclick('text=hcs');
+    await page.click('.far.fa-compass');
+    await page.waitForTimeout(3000);
+    await page.screenshot({ path: 'screenshot1.png'});
+    await page.click('text=plane 2 >> i');
+    await page.waitForTimeout(3000);
+    await page.screenshot({ path: 'screenshot2.png'});
+
+    //check differents of two images
+
+    const fs = require('fs');
+    const PNG = require('pngjs').PNG;
+    const pixelmatch = require('pixelmatch');
+    const img1 = PNG.sync.read(fs.readFileSync('screenshot1.png'));
+    const img2 = PNG.sync.read(fs.readFileSync('screenshot2.png'));
+    const {width, height} = img1;
+    const diff = new PNG({width, height});
+    pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.1});
+    fs.writeFileSync('diff.png', PNG.sync.write(diff));
+    
+})
+
+test.afterAll( async({browser}) => {
+    
     await browser.close();
 
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Login test
-
-/*
-test('login' , async ({page}) => {
-    
-  
-    await page.goto('https://showroom.arivis.com/#/login');
-    await page.fill('#input-15', 'nazafarin');
-    await page.fill('#input-18', 'arivis');
-    await page.click('text=LOGIN');
-    await expect(page).toHaveURL('https://showroom.arivis.com/#/');
-    await page.click('div.v-card__title');
-    await expect(page).toHaveURL('https://showroom.arivis.com/#/datasets');
-
-    await page.click('button.a-toolbar_left-action-menu_activator');
-    await page.click('text=Mount existing dataset');
-    await expect(page).toHaveURL('https://showroom.arivis.com/#/datasets/mount');
-    await page.dblclick('text=_testdata');
-    await page.dblclick('text=_nativeformats');
-    await page.dblclick('text=czi');
-    await page.dblclick('text=hcs');
-    await page.click('text=next');
-    await page.click('text=MOUNT DATASETS');
-    await page.waitForSelector('span.success');
-    await page.goto('https://showroom.arivis.com/#/datasets');
-    
-    await page.dblclick('text=hcs');
-    await page.dblclick('text=hcs');
-    await expect(page).toHaveURL(imgUrl);
-    await page.click('i.fa-palette');
-    await page.screenshot({ path: 'screenshot5.png'});
-
-    
-}); */
-  
   
